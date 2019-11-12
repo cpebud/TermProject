3e006baf2082ee815089b4796748b8f47dbab4c3
KermiteO <kermicheo@gmail.com>
2019-11-11 18:05:14 -0800

    Update GameBoard.java
    with resetSpace

--- TicTacToe/src/game/GameBoard.java
+++ TicTacToe/src/game/GameBoard.java
@@ -7,10 +7,11 @@
  * 					Autumn Nguyen,
  * 					Thomas Pridy
  * 
- * Copyright © 2019. All rights reserved.
+ * Copyright Â© 2019. All rights reserved.
  ******************************************************************************/
 package game;
 
+import java.util.*;
 import game.buttons.GameTile;
 import game.buttons.GameTile.Symbol;
 
@@ -27,7 +28,6 @@ public class GameBoard
     private TicTacToe game;
     
     private GameTile[] tiles = new GameTile[NUM_TILES];
-
     /***************************************************************************
      *      CONSTRUCTOR
      **************************************************************************/
@@ -94,5 +94,71 @@ public class GameBoard
             }
         }
         return true;
+    }
+    
+   
+  
+    public Boolean isWinner(Symbol s)
+    {
+    	//Boolean Variable
+    	boolean winner = false;
+    	//Check Row Winners
+    	if ((getTile(0).getTileSymbol() == s) && (getTile(1).getTileSymbol() == s) && (getTile(2).getTileSymbol() == s))
+		{
+			winner = true;
+		}
+    	else if ((getTile(3).getTileSymbol() == s) && (getTile(4).getTileSymbol() == s) && (getTile(5).getTileSymbol() == s))
+    	{
+			winner = true;
+    	}
+    	else if ((getTile(6).getTileSymbol() == s) && (getTile(7).getTileSymbol() == s) && (getTile(8).getTileSymbol() == s))
+    	{
+			winner = true;
+    	}
+    	//Check Column Winners
+    	else if ((getTile(0).getTileSymbol() == s) && (getTile(3).getTileSymbol() == s) && (getTile(6).getTileSymbol() == s))
+		{
+			winner = true;
+		}
+    	else if ((getTile(1).getTileSymbol() == s) && (getTile(4).getTileSymbol() == s) && (getTile(7).getTileSymbol() == s))
+    	{
+			winner = true;
+    	}
+    	else if ((getTile(2).getTileSymbol() == s) && (getTile(5).getTileSymbol() == s) && (getTile(8).getTileSymbol() == s))
+    	{
+			winner = true;
+    	}
+    	//Check Diagonal Winners
+    	else if ((getTile(0).getTileSymbol() == s) && (getTile(4).getTileSymbol() == s) && (getTile(8).getTileSymbol() == s))
+		{
+			winner = true;
+		}
+    	else if ((getTile(2).getTileSymbol() == s) && (getTile(4).getTileSymbol() == s) && (getTile(6).getTileSymbol() == s))
+    	{
+			winner = true;
+    	}
+    	
+        return winner;
+    }
+    
+    //for minimax alg
+// public GameTile[] getAvailableSpaces()
+//    {
+//    	GameTile[] availSpaces = new GameTile[9];
+//    	int totAvailableSp = 0;
+//    	for(int i = 0; i < 9; i++)
+//    	{    		
+//    		if(game.getBoard().getTile(i).isEmpty())
+//    			totAvailableSp++;
+//    	}
+//    	
+//    	
+//    	
+//    	return availTiles;
+//    }
+    
+    public void resetSpace(int space)
+    {
+    	tiles[space].setTileSymbol(Symbol.EMPTY);   	
     }
 }
